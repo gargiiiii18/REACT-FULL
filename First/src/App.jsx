2a39c53a7feae77a9ec8a5ded358f6ac06df0032
Abhishe16 <ankushpan694@gmail.com>
2024-07-11 21:24:53 +0530

    made a button component

--- First/src/App.jsx
+++ First/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header.jsx"
 import Food from "./Food.jsx"
 import Footer from "./Footer.jsx"
 import Card from "./Card.jsx"
+import Button from "./Button.jsx"
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
     <Food/>
     <Footer/>
     <Card/>
+    <br></br>
+    <Button/>
     
 
     </>
