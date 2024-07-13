7c991d4b45cbdd0f4f925d3c81060f88f60cb497
Abhishe16 <ankushpan694@gmail.com>
2024-07-13 12:16:30 +0530

    updated

--- First/src/UserGreetings.jsx
+++ First/src/UserGreetings.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types";
 function UserGreetings(props){
     
         if(props.isLoggedIn){
-            return <h2>Welcome {props.username}</h2>
+            return <h2 className="welcome-message">Welcome {props.username}</h2>
         }
         else{
-            return <h2>Please log in to continue</h2>
+            return <h2 className="login-prompt">Please log in to continue</h2>
         }
     
 }
