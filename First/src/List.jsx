36a696035c5577548c6199ce6c179de6145868c1
Abhishe16 <ankushpan694@gmail.com>
2024-07-14 19:05:49 +0530

    sorted fruit list in descending order of their calorie values

--- First/src/List.jsx
+++ First/src/List.jsx
@@ -3,9 +3,20 @@ function List() {
         {name: "banana", calories : 105}, 
         {name: "orange", calories : 69}, 
         {name: "watermelon", calories: 70}];
+
+    // Sorting the fruits --> 1. Ascending 2. Descending
+    // fruits.sort((a,b) => a.name.localeCompare(b.name)); --> Ascending Order
+    // fruits.sort((a,b) => b.name.localeCompare(a.name));
+    fruits.sort((a,b) => b.calories - a.calories);
+    // fruits.sort((a,b) => a.calories - b.calories);  Ascending order of calories value
+    
+
+
     const listItems = fruits.map((fruit, index) => <li key={index}>{fruit.name}: &nbsp;
                                                        <b>{fruit.calories}</b></li>);
     return (<ol>{listItems}</ol>);
+
+    
 }
 
 export default List;
