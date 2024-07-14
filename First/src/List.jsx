65e963ff6d8c61eb0f8e9b02453eceb6822db472
Abhishe16 <ankushpan694@gmail.com>
2024-07-14 18:26:43 +0530

    used list rendering properties to display fruit list

--- First/src/List.jsx
+++ First/src/List.jsx
@@ -1,7 +1,10 @@
-
-function List(){
-    const fruits = ["apple", "banana", "orange", "watermelon"];
-    return (fruits);
+function List() {
+    const fruits = [{name: "apple", calories : 95}, 
+        {name: "banana", calories : 105}, 
+        {name: "orange", calories : 69}, 
+        {name: "watermelon", calories: 70}];
+    const listItems = fruits.map((fruit, index) => <li key={index}>{fruit.name}</li>);
+    return (<ol>{listItems}</ol>);
 }
 
 export default List;
