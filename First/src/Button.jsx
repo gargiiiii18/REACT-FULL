1ed998c545a16e90b908d7fd9f2fee30e1ce4021
Abhishe16 <ankushpan694@gmail.com>
2024-07-31 20:19:47 +0530

    made changes

--- First/src/Button.jsx
+++ First/src/Button.jsx
@@ -1,8 +1,11 @@
 function Button() {
-    const handleClick = () => alert("OUCH🤣");
+    // const handleClick = () => alert("OUCH🤣");
+    const handleClick2 = () => alert(`${name} stop clicking me`)
 return(
-    
-    <button onClick={handleClick} className="button">Dabao👩‍💻!!!</button>
+    <>
+    {/* <button onClick={handleClick} className="button">Dabao👩‍💻!!!</button> */}
+    <button onClick={() => handleClick2(prompt("Enter your name", name))}className="button" >Dabao👩‍💻!!!</button>
+    </>
 
 );
 }
