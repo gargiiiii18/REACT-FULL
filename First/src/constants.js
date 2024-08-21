cdb117c4318422ed58014e52495125bbf4f71a64
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2024-08-21 21:48:01 +0530

    constants added

---
+++ First/src/constants.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const people=[
+    {
+        name: "Gargi",
+        info: "Web Developer trying to build unique websites and web apps.",
+        img: "https://thumbs.dreamstime.com/b/cyberpunk-hacker-anime-girl-edgy-haircuy-typing-holographic-keyboard-surrounded-neon-code-digital-data-277372317.jpg"
+    },
+    
+    {
+        name: "Dawg",
+        info: "Hacker trying to hack unique websites and web apps.",
+        img: "https://azuretracks.com/wp-content/uploads/2023/11/bf7fdc0b-6600-42d7-8b9f-fb898eac01e4_used-350x270.jpg"
+    },
+    
+    {
+        name: "Kitty",
+        info: "App developer trying to build unique mobile applications.",
+        img: "https://img.freepik.com/premium-photo/cat-computer_815088-234.jpg"
+    },   
+]
+
+const students=[
+    {
+    name: "Kobbie Mainoo",
+    age: 19,
+    position: "CDM",
+    isBestYoungPlayer: true
+    },
+    {
+    name: "Alejandro Garnacho",
+    age: 20,
+    position: "CDM",
+    isBestYoungPlayer: true
+    },
+    {
+    name: "Rasmus Højlund",
+    age: 21,
+    position: "ST",
+    isBestYoungPlayer: false
+    },
+
+]
+
+export {people, students};
